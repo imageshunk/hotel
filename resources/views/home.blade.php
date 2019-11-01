e108f10622a7a9e1a1709d9a4c6a45e252762e4a
ImagesHunkAdmin <ceo@nethunk.com>
2019-11-01 16:24:40 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,7 +85,7 @@
     </div>
 
     <div class="row">
-        <div class="col-sm-3">
+        <div class="col-sm-6">
             <div class="box box-danger">
                 <div class="box-header with-border">
                     <h3 class="box-title">New Guests</h3>
@@ -118,8 +118,51 @@
                 <!-- /.box-footer -->
             </div>
         </div>
+        <div class="col-sm-6>
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Recently Added Items</h3>
 
-        <div class="col-sm-5">
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <ul class="products-list product-list-in-box">
+                        <?php $items = App\Item::latest()->limit(5)->get(); ?>
+                        @foreach($items as $item)
+                            <li class="item">
+                                <div class="product-img">
+                                    <i class="fa fa-utensils"></i>
+                                </div>
+                                <div class="product-info">
+                                    <?php $setting = App\Setting::first(); ?>
+                                    <a href="javascript:void(0)" class="product-title">{{$item->item_name}}
+                                    <span class="label label-warning pull-right">{{$setting->currency}} {{$item->item_price}}</span></a>
+                                    <span class="product-description">
+                                        <?php $category = App\Category::find($item->item_category); ?>
+                                        {{$category->category}}
+                                        @if($item->qty != NULL)
+                                            , Quantity: {{$item->qty}}
+                                        @endif
+                                    </span>
+                                </div>
+                            </li>
+                        @endforeach
+                        <!-- /.item -->
+                    </ul>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer text-center">
+                    <a href="/items" class="uppercase">View All Products</a>
+                </div>
+                <!-- /.box-footer -->
+            </div>
+        </div>
+        <div class="col-sm-12">
             <div class="box box-info">
                 <div class="box-header with-border">
                     <h3 class="box-title">Latest Orders</h3>
@@ -163,50 +206,6 @@
                 <div class="box-footer clearfix">
                 <a href="/room-service" class="btn btn-sm btn-info btn-flat pull-left">Room Service</a>
                 <a href="/orders" class="btn btn-sm btn-default btn-flat pull-right">View All Orders</a>
-                </div>
-                <!-- /.box-footer -->
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Recently Added Items</h3>
-
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                    </div>
-                </div>
-                <!-- /.box-header -->
-                <div class="box-body">
-                    <ul class="products-list product-list-in-box">
-                        <?php $items = App\Item::latest()->limit(5)->get(); ?>
-                        @foreach($items as $item)
-                            <li class="item">
-                                <div class="product-img">
-                                    <i class="fa fa-utensils"></i>
-                                </div>
-                                <div class="product-info">
-                                    <?php $setting = App\Setting::first(); ?>
-                                    <a href="javascript:void(0)" class="product-title">{{$item->item_name}}
-                                    <span class="label label-warning pull-right">{{$setting->currency}} {{$item->item_price}}</span></a>
-                                    <span class="product-description">
-                                        <?php $category = App\Category::find($item->item_category); ?>
-                                        {{$category->category}}
-                                        @if($item->qty != NULL)
-                                            , Quantity: {{$item->qty}}
-                                        @endif
-                                    </span>
-                                </div>
-                            </li>
-                        @endforeach
-                        <!-- /.item -->
-                    </ul>
-                </div>
-                <!-- /.box-body -->
-                <div class="box-footer text-center">
-                    <a href="/items" class="uppercase">View All Products</a>
                 </div>
                 <!-- /.box-footer -->
             </div>
